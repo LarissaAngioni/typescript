1a54cc43647f5326a3071541f8796256393cb6d2
Larissa Angioni <larissaangioni@gmail.com>
2024-07-10 11:52:39 -0300

    Aula Exceptions

---
+++ exceptions_ts/Conversor.ts
@@ -0,0 +1,18 @@
+﻿
+try {
+    const resultado = converte("abc");
+    console.log(resultado);
+
+} catch (error: any) {
+    if (error instanceof TypeError)
+        console.error("Você digitou um número. Digite um texto e tente novamente!");
+    else
+        console.error("Erro inesperado! Contate o adm do sistema.");
+} finally {
+    console.log("Fim do código!");
+    
+}
+
+export function converte(conteudo: any): string {
+    return conteudo.toUpperCase();
+}
